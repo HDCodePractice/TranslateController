aaa97514852b4369f9725a194f9a5a6dd7bce32c
老房东 <huangdong@gmail.com>
2022-01-03 09:44:53 -0500

    init version

---
+++ README.md
@@ -0,0 +1,3 @@
+# TranslateController : A SwiftUI package to calling the iOS translation function
+
+* iOS 15+
---
+++ Sources/TranslateController/TranslateController.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+import UIKit
+
+public struct TranslateController: UIViewControllerRepresentable {
+    @Binding var text : String
+    @Binding var showing : Bool
+    
+    public typealias UIViewControllerType = UINavigationController
+    
+    public func makeUIViewController(context: Context) -> UINavigationController {
+        let navController =  UINavigationController()
+        navController.setNavigationBarHidden(true, animated: false)
+        let viewController = UIViewController()
+        navController.addChild(viewController)
+        return navController
+    }
+    
+    public func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {
+        if showing {
+            translate(uiViewController,text: text)
+        }
+    }
+    
+    func translate(_ navController:UINavigationController , text: String) {
+        guard !text.isEmpty else {
+            return
+        }
+        if #available(iOS 15.0, *) {
+            let text = text.unicodeScalars.filter { !$0.properties.isEmojiPresentation}.reduce("") { $0 + String($1) }
+            
+            let textView = UITextView()
+            textView.text = text
+            textView.isEditable = false
+            if let topController = navController.topViewController{
+                topController.view.addSubview(textView)
+                textView.selectAll(nil)
+                textView.perform(NSSelectorFromString(["_", "trans", "late:"].joined(separator: "")), with: nil)
+                
+                DispatchQueue.main.async {
+                    textView.removeFromSuperview()
+                    showing = false
+                }
+            }
+        }
+    }
+}
+
